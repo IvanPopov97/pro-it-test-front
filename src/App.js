548421eb2cdfeae0081f3e4de8931caa32e235fd
Popov Ivan <p77599478113@yandex.ru>
2020-06-11 05:29:11 +0500

    Second initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="Table-container">
+        <table className="Flex-table">
+          <thead>
+          <tr>
+            <th>ID</th>
+            <th>Название компании</th>
+            <th>Количество сотрудников</th>
+            <th>Головная компания</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td><span>ID</span>1</td>
+            <td><span>Название компании</span>Perm State University</td>
+            <td><span>Количество сотрудников</span>19563</td>
+            <td><span>Головная компания</span></td>
+          </tr>
+          <tr>
+            <td><span>ID</span>2</td>
+            <td><span>Название компании</span>Мегафон</td>
+            <td><span>Количество сотрудников</span>963475</td>
+            <td><span>Головная компания</span>Yota</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
   );
 }
 
