10d72e689e560dca1f0dfccfa632680eade3ce3e
Popov Ivan <p77599478113@yandex.ru>
2020-06-14 05:36:50 +0500

    Добавил Роутинг

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainNavigation from './MainNavigation'
+import Content from './Content'
+
+function App() {
+
+  return (
+      <div className='Page'>
+          <MainNavigation/>
+          <Content/>
+      </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {rootReducer} from './redux/rootReducer'
+import './index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
+
+const commonStore = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={commonStore}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
